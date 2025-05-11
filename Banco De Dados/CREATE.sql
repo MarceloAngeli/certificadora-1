eeb1698935d2b3d402c30445fb502e3044c09064
AugustoF <ap.fiorucci@gmail.com>
2025-05-11 00:31:50 -0300

    Atualiza script com atributos e tabelas principais do banco de dados

--- Banco De Dados/CREATE.sql
+++ Banco De Dados/CREATE.sql
@@ -2,20 +2,34 @@ CREATE DATABASE IF NOT EXISTS plataforma_noticia;
 USE plataforma_noticia;
 
 CREATE TABLE IF NOT EXISTS usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(225) NOT NULL UNIQUE,
-    telefone INT NOT NULL UNIQUE,
-    senha_hash INT NOT NULL
+    idade INT NOT NULL,
+    telefone VARCHAR(20) NOT NULL UNIQUE,
+    sobre TEXT,
+    senha_hash VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS noticia(
-	 titulo VARCHAR(200) NOT NULL
+CREATE TABLE IF NOT EXISTS noticia (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(200) NOT NULL,
+    imagem MEDIUMBLOB,
+    text_noticia TEXT
 );
 
-CREATE TABLE IF NOT EXISTS comentario(
-	conteudo TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS comentario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    conteudo TEXT NOT NULL,
+    id_usuario INT NOT NULL,
+    id_noticia VARCHAR(100) NOT NULL,
+    FOREIGN KEY (id_usuario)
+        REFERENCES usuario (id),
+    FOREIGN KEY (id_noticia)
+        REFERENCES noticia (id)
 );
 
-CREATE TABLE IF NOT EXISTS insc_notificacao(
-	 tipo_notificacao VARCHAR(50) NOT NULL
+CREATE TABLE IF NOT EXISTS insc_notificacao (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tipo_notificacao VARCHAR(50) NOT NULL
 );

13b6bb3ceb7031807911669ba29d79a15e55e819
João H. L. Corrêa <jhenriquelc@gmail.com>
2025-06-11 13:28:49 -0300

    Modularizar componentes da landing page

--- Sistema/src/routes/+page.svelte
+++ Sistema/src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
   import NavBar from '$lib/NavBar.svelte';
-  import HeroSection from '$lib/HeroSection.svelte';
-  import ObjetivoSection from '$lib/ObjetivoSection.svelte';
-  import HistoriaSection from '$lib/HistoriaSection.svelte';
-  import BlogCarousel from '$lib/BlogCarousel.svelte';
-  import VolunteerForm from '$lib/VolunteerForm.svelte';
+  import HeroSection from '$lib/landing_page/HeroSection.svelte';
+  import ObjetivoSection from '$lib/landing_page/ObjetivoSection.svelte';
+  import HistoriaSection from '$lib/landing_page/HistoriaSection.svelte';
+  import BlogCarousel from '$lib/landing_page/BlogCarousel.svelte';
+  import VolunteerForm from '$lib/landing_page/VolunteerForm.svelte';
   import NewsletterForm from '$lib/NewsletterForm.svelte';
   import FooterSection from '$lib/FooterSection.svelte';
 </script>
